97e5762486adf227a7fc373fcb48febed3016e40
Keira Aro <sylae@calref.net>
2018-07-07 03:24:01 -0700

    KEIRA YOUR PING SUCKS
    WHAT NOW GAIA WHAT NOW :triumph:

--- src/Plugin/Ping.php
+++ src/Plugin/Ping.php
@@ -25,7 +25,19 @@ class Ping implements \Huntress\PluginInterface
     public static function process(\Huntress\Bot $bot, \CharlotteDunois\Yasmin\Models\Message $message): \React\Promise\ExtendedPromiseInterface
     {
         try {
-            return self::send($message->channel, "Pong!");
+            $message_tx = \Carbon\Carbon::createFromTimestampMs(round(microtime(true) * 1000));
+            $dstamp_tx  = \Carbon\Carbon::createFromTimestampMs(\CharlotteDunois\Yasmin\Utils\Snowflake::deconstruct($message->id)->timestamp * 1000);
+            return self::send($message->channel, "Pong!")->then(function (\CharlotteDunois\Yasmin\Models\Message $message) use ($message_tx, $dstamp_tx) {
+                        $message_rx = \Carbon\Carbon::createFromTimestampMs(round(microtime(true) * 1000));
+                        $dstamp_rx  = \Carbon\Carbon::createFromTimestampMs(\CharlotteDunois\Yasmin\Utils\Snowflake::deconstruct($message->id)->timestamp * 1000);
+
+                        $v = [
+                            number_format(($message_rx->format("U.u") - $message_tx->format("U.u")) * 1000),
+                            number_format(($dstamp_rx->format("U.u") - $dstamp_tx->format("U.u")) * 1000),
+                        ];
+
+                        $message->edit(vsprintf("Pong!\n%sms ping (huntress-rx)\n%sms ping (msg-snowflake)", $v));
+                    });
         } catch (\Throwable $e) {
             return self::exceptionHandler($message, $e);
         }
