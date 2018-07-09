3df1eaae676bac88714cce92b2736e1a72f61fc8
Keira Aro <sylae@calref.net>
2018-07-09 04:14:12 -0700

    provide feedback while the fic search is going

--- src/Plugin/FanficLibrary.php
+++ src/Plugin/FanficLibrary.php
@@ -127,41 +127,47 @@ class FanficLibrary implements \Huntress\PluginInterface
     public static function lookup(\Huntress\Bot $bot, \CharlotteDunois\Yasmin\Models\Message $message): \React\Promise\ExtendedPromiseInterface
     {
         try {
-            $args   = self::_split($message->content);
-            $search = trim(str_replace($args[0], "", $message->content));
-            $user   = $message->member;
+            $time  = \Carbon\Carbon::createFromTimestampMs(round(microtime(true) * 1000));
+            $count = number_format(count(self::$library));
+            return self::send($message->channel, ":crystal_ball: Searching **$count** stories...")->then(function (\CharlotteDunois\Yasmin\Models\Message $sent) use ($message, $time) {
+                $args   = self::_split($message->content);
+                $search = trim(str_replace($args[0], "", $message->content));
+                $user   = $message->member;
 
-            $res = self::$library->titleSearch($search, 10);
+                $res = self::$library->titleSearch($search, 10);
 
-            $embed = self::easyEmbed($message);
-            $embed->setTitle("Browsing stories matching: `$search`")
-            ->setDescription("For more details, use `!find [FIC_NAME]`")
-            ->setColor($user->displayColor);
-            foreach ($res as $k => $v) {
-                $title = [];
-                $data  = [];
+                $embed = self::easyEmbed($message);
+                $embed->setTitle("Browsing stories matching: `$search`")
+                ->setDescription("For more details, use `!find [FIC_NAME]`")
+                ->setColor($user->displayColor);
+                foreach ($res as $k => $v) {
+                    $title = [];
+                    $data  = [];
 
-                $title[] = "{$v->title}";
-                if (mb_strlen(trim($v->author ?? "")) > 0) {
-                    $title[] = "*by {$v->author}*";
-                }
-                if (mb_strlen(trim($v->status ?? "")) > 0) {
-                    $title[] = "*({$v->status})*";
-                }
-                if ($v->words ?? 0 > 0) {
-                    $data[] = "*(" . number_format($v->words) . " words)*";
-                }
-                if (mb_strlen(trim($v->comments ?? "")) > 0) {
-                    $data[] = "*(" . self::htmlToMD((string) $v->comments) . ")*";
-                }
-                if (count($v->tags ?? []) > 0) {
-                    $data[] = "\n__Tagged__: " . implode(", ", $v->tags);
-                }
-                $data[] = "\n";
+                    $title[] = "{$v->title}";
+                    if (mb_strlen(trim($v->author ?? "")) > 0) {
+                        $title[] = "*by {$v->author}*";
+                    }
+                    if (mb_strlen(trim($v->status ?? "")) > 0) {
+                        $title[] = "*({$v->status})*";
+                    }
+                    if ($v->words ?? 0 > 0) {
+                        $data[] = "*(" . number_format($v->words) . " words)*";
+                    }
+                    if (mb_strlen(trim($v->comments ?? "")) > 0) {
+                        $data[] = "*(" . self::htmlToMD((string) $v->comments) . ")*";
+                    }
+                    if (count($v->tags ?? []) > 0) {
+                        $data[] = "\n__Tagged__: " . implode(", ", $v->tags);
+                    }
+                    $data[] = "\n";
 
-                $embed->addField(implode(" ", $title), implode(" ", $data));
-            }
-            return self::send($message->channel, "", ['embed' => $embed]);
+                    $embed->addField(implode(" ", $title), implode(" ", $data));
+                }
+                $spent = number_format((\Carbon\Carbon::createFromTimestampMs(round(microtime(true) * 1000))->format("U.u") - $time->format("U.u")), 1);
+                $count = number_format(count(self::$library));
+                return $sent->edit("Searched **$count** records in {$spent} seconds.", ['embed' => $embed]);
+            });
         } catch (\Throwable $e) {
             return self::exceptionHandler($message, $e);
         }
