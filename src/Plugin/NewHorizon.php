dbd8ec7d6f0ca9417c8c73dccabdb14a061c6af6
Keira Aro <sylae@calref.net>
2018-08-10 12:10:01 -0700

    configurable welcome message

--- src/Plugin/NewHorizon.php
+++ src/Plugin/NewHorizon.php
@@ -20,6 +20,65 @@ class NewHorizon implements \Huntress\PluginInterface
     public static function register(\Huntress\Bot $bot)
     {
         $bot->client->on("voiceStateUpdate", [self::class, "voiceStateHandler"]);
+        $bot->client->on("guildMemberAdd", [self::class, "guildMemberAddHandler"]);
+        $bot->client->on(self::PLUGINEVENT_COMMAND_PREFIX . "_NHInternalSetWelcomeMessage", [self::class, "setWelcome"]);
+        $bot->client->on(self::PLUGINEVENT_DB_SCHEMA, [self::class, "db"]);
+    }
+
+    public static function db(\Doctrine\DBAL\Schema\Schema $schema): void
+    {
+        $t = $schema->createTable("nh_config");
+        $t->addColumn("key", "string", ['customSchemaOptions' => \Huntress\DatabaseFactory::CHARSET]);
+        $t->addColumn("value", "text", ['customSchemaOptions' => \Huntress\DatabaseFactory::CHARSET]);
+        $t->setPrimaryKey(["key"]);
+    }
+
+    public static function setWelcome(\Huntress\Bot $bot, \CharlotteDunois\Yasmin\Models\Message $message): \React\Promise\ExtendedPromiseInterface
+    {
+        if (is_null($message->member->roles->get("450658242125627402"))) {
+            return self::unauthorized($message);
+        } else {
+            try {
+                $args = self::_split($message->content);
+                if (count($args) < 2) {
+                    return self::error($message, "You dipshit :open_mouth:", "!_NHInternalSetWelcomeMessage This is where you put the message\n%s = username");
+                }
+                $welcomeMsg = trim(str_replace($args[0], "", $message->content));
+
+
+                $query = \Huntress\DatabaseFactory::get()->prepare('INSERT INTO nh_config (`key`, `value`) VALUES(?, ?) '
+                . 'ON DUPLICATE KEY UPDATE `value`=VALUES(`value`);', ['string', 'string']);
+                $query->bindValue(1, "serverWelcomeMessage");
+                $query->bindValue(2, $welcomeMsg);
+                $query->execute();
+
+                return self::send($message->channel, self::formatWelcomeMessage($message->author));
+            } catch (\Throwable $e) {
+                return self::exceptionHandler($message, $e, true);
+            }
+        }
+    }
+
+    public static function guildMemberAddHandler(\CharlotteDunois\Yasmin\Models\GuildMember $member): \React\Promise\ExtendedPromiseInterface
+    {
+        var_dump($member->user->tag);
+        return self::send($member->guild->channels->get("450691718359023616"), self::formatWelcomeMessage($member->user));
+    }
+
+    private static function formatWelcomeMessage(\CharlotteDunois\Yasmin\Models\User $member)
+    {
+        return sprintf(self::getWelcomeMessage(), (string) $member);
+    }
+
+    private static function getWelcomeMessage(): string
+    {
+        $qb  = \Huntress\DatabaseFactory::get()->createQueryBuilder();
+        $qb->select("*")->from("nh_config")->where('`key` = ?')->setParameter(0, 'serverWelcomeMessage', "string");
+        $res = $qb->execute()->fetchAll();
+        foreach ($res as $data) {
+            return $data['value'];
+        }
+        return "Welcome to New Horizon!";
     }
 
     public static function voiceStateHandler(\CharlotteDunois\Yasmin\Models\GuildMember $new, ?\CharlotteDunois\Yasmin\Models\GuildMember $old)
