d506f8ec7530b89c132f72a12b4ea6887ad809b9
Keira Dueck <sylae@calref.net>
2018-12-13 02:43:42 -0800

    formatting

--- src/Plugin/PCT.php
+++ src/Plugin/PCT.php
@@ -40,38 +40,38 @@ class PCT implements \Huntress\PluginInterface
 
     public static function sbHell(\Huntress\Bot $bot)
     {
-        $bot->loop->addPeriodicTimer(60, function() use ($bot) {
+        $bot->loop->addPeriodicTimer(60, function () use ($bot) {
             if (php_uname('s') == "Windows NT") {
                 return null; // don't run on testing because oof
             }
-            return \CharlotteDunois\Yasmin\Utils\URLHelpers::resolveURLToData("https://forums.spacebattles.com/forums/worm.115/")->then(function(string $string) use ($bot) {
-                $data	 = \html5qp($string);
-                $items	= $data->find('li.discussionListItem');
+            return \CharlotteDunois\Yasmin\Utils\URLHelpers::resolveURLToData("https://forums.spacebattles.com/forums/worm.115/")->then(function (string $string) use ($bot) {
+                $data  = \html5qp($string);
+                $items = $data->find('li.discussionListItem');
                 foreach ($items as $item) {
-                    $x = (object) [
-                        'id' => (int) str_replace("thread-", "", $item->attr("id")),
-                        'title' => trim($item->find('h3')->text()),
+                    $x     = (object) [
+                        'id'         => (int) str_replace("thread-", "", $item->attr("id")),
+                        'title'      => trim($item->find('h3')->text()),
                         'threadTime' => self::unfuckDates($item->find(".startDate .DateTime")),
-                        'replyTime' => self::unfuckDates($item->find(".lastPost .DateTime")),
-                        'author' => [
+                        'replyTime'  => self::unfuckDates($item->find(".lastPost .DateTime")),
+                        'author'     => [
                             'name' => $item->find('.posterDate a.username')->text(),
-                            'av' => "https://forums.spacebattles.com/" . $item->find('.posterAvatar img')->attr("src"),
-                            'url' => "https://forums.spacebattles.com/" . $item->find('.posterDate a.username')->attr("href"),
+                            'av'   => "https://forums.spacebattles.com/" . $item->find('.posterAvatar img')->attr("src"),
+                            'url'  => "https://forums.spacebattles.com/" . $item->find('.posterDate a.username')->attr("href"),
                         ],
-                        'replier' => [
+                        'replier'    => [
                             'name' => $item->find('.lastPost a.username')->text(),
-                            'av' => null,
+                            'av'   => null,
                         ],
                         'numReplies' => (int) trim($item->find('.stats .major dd')->text()),
-                        'numViews' => (int) str_replace(",", "", $item->find('.stats .minor dd')->text()),
-                        'wordcount' => str_replace("Word Count: ", "", $item->find(".posterDate a.OverlayTrigger")->text()),
+                        'numViews'   => (int) str_replace(",", "", $item->find('.stats .minor dd')->text()),
+                        'wordcount'  => str_replace("Word Count: ", "", $item->find(".posterDate a.OverlayTrigger")->text()),
                     ];
                     $embed = new \CharlotteDunois\Yasmin\Models\MessageEmbed();
                     $embed->setTitle($x->title)->setColor(0x00ff00)
                     ->setURL("https://forums.spacebattles.com/threads/{$x->id}/")
                     ->setAuthor($x->author['name'], $x->author['av'], $x->author['url'])
                     ->addField("Created", $x->threadTime->toFormattedDateString(), true)
-                    ->addField("Replies", sprintf("%s (%s pages)", number_format($x->numReplies), number_format(ceil($x->numReplies/25))), true)
+                    ->addField("Replies", sprintf("%s (%s pages)", number_format($x->numReplies), number_format(ceil($x->numReplies / 25))), true)
                     ->addField("Views", number_format($x->numViews), true)
                     ->setFooter("Last reply")
                     ->setTimestamp($x->replyTime->timestamp);
@@ -100,17 +100,18 @@ class PCT implements \Huntress\PluginInterface
         if (self::isTestingClient()) {
             return;
         }
-        $bot->loop->addPeriodicTimer(60, function() use ($bot) {
-            return \CharlotteDunois\Yasmin\Utils\URLHelpers::resolveURLToData("https://www.reddit.com/r/wormfanfic/new.json")->then(function(string $string) use ($bot) {
+        $bot->loop->addPeriodicTimer(60, function () use ($bot) {
+            return \CharlotteDunois\Yasmin\Utils\URLHelpers::resolveURLToData("https://www.reddit.com/r/wormfanfic/new.json")->then(function (string $string) use ($bot) {
                 try {
                     $items    = json_decode($string)->data->children;
                     $lastPub  = self::getLastRSS();
                     $newest   = $lastPub;
                     $newItems = [];
                     foreach ($items as $item) {
-                        $published  = (int) $item->data->created_utc;
-                        if ($published <= $lastPub)
+                        $published = (int) $item->data->created_utc;
+                        if ($published <= $lastPub) {
                             continue;
+                        }
                         $newest     = max($newest, $published);
                         $newItems[] = (object) [
                             'title'    => $item->data->title,
@@ -125,9 +126,9 @@ class PCT implements \Huntress\PluginInterface
                         if (mb_strlen($item->body) > 512) {
                             $item->body = substr($item->body, 0, 509) . "...";
                         }
-                        $channel    = $bot->client->channels->get(466074264731385876);
-                        $embed      = new \CharlotteDunois\Yasmin\Models\MessageEmbed();
-                        $embed->setTitle($item->title)->setURL($item->link)->setDescription($item->body)->setTimestamp($item->date->timestamp)->setFooter($item->category)->setAuthor($item->author, '', "https://reddit.com/user/".$item->author);
+                        $channel = $bot->client->channels->get(466074264731385876);
+                        $embed   = new \CharlotteDunois\Yasmin\Models\MessageEmbed();
+                        $embed->setTitle($item->title)->setURL($item->link)->setDescription($item->body)->setTimestamp($item->date->timestamp)->setFooter($item->category)->setAuthor($item->author, '', "https://reddit.com/user/" . $item->author);
                         $channel->send("", ['embed' => $embed]);
                     }
                     $query = \Huntress\DatabaseFactory::get()->prepare('INSERT INTO pct_config (`key`, `value`) VALUES(?, ?) '
@@ -148,22 +149,22 @@ class PCT implements \Huntress\PluginInterface
      * @return \Carbon\Carbon
      * @throws \LogicException
      */
-     private static function unfuckDates(\QueryPath\DOMQuery $qp): \Carbon\Carbon
-     {
-     	if ($qp->is("span")) {
-     		$obj = new \Carbon\Carbon(str_replace(" at", "", $qp->text()), "America/New_York");
-     	} elseif ($qp->is("abbr")) {
-     		$obj = new \Carbon\Carbon(date('c', $qp->attr("data-time")));
-     	} else {
-     		throw new \LogicException("what the fuck");
-     	}
-     	$obj->setTimezone("UTC");
-     	return $obj;
-     }
+    private static function unfuckDates(\QueryPath\DOMQuery $qp): \Carbon\Carbon
+    {
+        if ($qp->is("span")) {
+            $obj = new \Carbon\Carbon(str_replace(" at", "", $qp->text()), "America/New_York");
+        } elseif ($qp->is("abbr")) {
+            $obj = new \Carbon\Carbon(date('c', $qp->attr("data-time")));
+        } else {
+            throw new \LogicException("what the fuck");
+        }
+        $obj->setTimezone("UTC");
+        return $obj;
+    }
 
     private static function alreadyPosted(\stdClass $post): bool
     {
-        $qb = \Huntress\DatabaseFactory::get()->createQueryBuilder();
+        $qb  = \Huntress\DatabaseFactory::get()->createQueryBuilder();
         $qb->select("*")->from("pct_sbhell")->where('`idTopic` = ?')->setParameter(0, $post->id, "integer");
         $res = $qb->execute()->fetchAll();
         foreach ($res as $data) {
