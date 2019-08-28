10e09a4ffe14497ceff98816975492a8e65a92b6
Keira Dueck <sylae@calref.net>
2019-08-28 02:32:59 -0700

    wormmemes

---
+++ src/Huntress/Plugin/RCB.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2019 Keira Dueck <sylae@calref.net>
+ * Use of this source code is governed by the MIT license, which
+ * can be found in the LICENSE file.
+ */
+
+namespace Huntress\Plugin;
+
+
+use Huntress\Huntress;
+use Huntress\PluginHelperTrait;
+use Huntress\PluginInterface;
+use Huntress\RedditProcessor;
+
+class RCB implements PluginInterface
+{
+    use PluginHelperTrait;
+
+    public static function register(Huntress $bot)
+    {
+        if (self::isTestingClient()) {
+            $bot->log->debug("Not adding RSS event on testing.");
+        } else {
+            new RedditProcessor($bot, "rcbWormMemes", "WormMemes", 30, 354769211937259521);
+        }
+    }
+}
