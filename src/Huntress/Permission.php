938a1ee02f26a98277f9ee203e6b40481139f349
Keira Dueck <sylae@calref.net>
2020-01-23 02:00:13 -0800

    wip save

---
+++ src/Huntress/Permission.php
@@ -0,0 +1,278 @@
+<?php
+
+/**
+ * Copyright (c) 2019 Keira Dueck <sylae@calref.net>
+ * Use of this source code is governed by the MIT license, which
+ * can be found in the LICENSE file.
+ */
+
+namespace Huntress;
+
+use CharlotteDunois\Collect\Collection;
+use CharlotteDunois\Yasmin\Interfaces\GuildChannelInterface;
+use CharlotteDunois\Yasmin\Models\Guild;
+use CharlotteDunois\Yasmin\Models\GuildMember;
+use CharlotteDunois\Yasmin\Models\Message;
+use CharlotteDunois\Yasmin\Models\Permissions;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * This handles all of the data related to a single permission or group
+ *
+ * @author Keira Dueck <sylae@calref.net>
+ */
+class Permission
+{
+    const SETTING_GLOBAL = 0;
+    const SETTING_GUILD = 1;
+    const SETTING_CHANNEL = 2;
+    const TARGET_GLOBAL = 0;
+    const TARGET_ROLE = 1;
+    const TARGET_USER = 2;
+    const TARGET_GUILDOWNER = 3;
+    const TARGET_BOTADMIN = 4;
+    /**
+     *
+     * @var string
+     */
+    public $title;
+    /**
+     * @var bool
+     */
+    public $default;
+    /**
+     *
+     * @var Collection
+     */
+    private $entries;
+    /**
+     *
+     * @var Huntress
+     */
+    private $huntress;
+    /**
+     *
+     * @var EventData
+     */
+    private $context;
+
+    public function __construct(string $permission, Huntress $huntress, bool $default = false)
+    {
+        $this->huntress = $huntress;
+        $this->title = $permission;
+        $this->default = $default;
+        $this->entries = $this->getAllPerms();
+        $this->context = new EventData();
+    }
+
+    private function getAllPerms(): Collection
+    {
+        $qb = $this->huntress->db->createQueryBuilder();
+        $qb->select("*")->from("permissions")->where("title = ?")->setParameter(1, $this->title, "string");
+        return new Collection($qb->execute()->fetchAll());
+    }
+
+    public static function register(Huntress $huntress)
+    {
+        $dbEv = EventListener::new()->addEvent("dbSchema")->setCallback([self::class, "db"]);
+        $huntress->eventManager->addEventListener($dbEv);
+    }
+
+    public static function db(Schema $schema): void
+    {
+        $t = $schema->createTable("permissions");
+        $t->addColumn("title", "string",
+            ['length' => 255, 'customSchemaOptions' => DatabaseFactory::CHARSET]);
+        $t->addColumn("value", "boolean", ['default' => true]);
+        $t->addColumn("settingType", "smallint", ["unsigned" => true]);
+        $t->addColumn("setting", "bigint", ["unsigned" => true]);
+        $t->addColumn("targetType", "smallint", ["unsigned" => true]);
+        $t->addColumn("target", "bigint", ["unsigned" => true, 'notnull' => false]);
+        $t->setPrimaryKey(["title", "settingType", "setting", "targetType", "target"]);
+        $t->addIndex(["title"]);
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getEntries(): Collection
+    {
+        return $this->entries;
+    }
+
+    public function addGuildContext(Guild $guild): Permission
+    {
+        $this->context->guild = $guild;
+        return $this;
+    }
+
+    public function addMemberContext(GuildMember $member): Permission
+    {
+        $this->context->user = $member;
+        $this->context->guild = $member->guild;
+        return $this;
+    }
+
+    public function addChannelContext(GuildChannelInterface $channel): Permission
+    {
+        $this->context->channel = $channel;
+        $this->context->guild = $channel->guild;
+        return $this;
+    }
+
+    public function addMessageContext(Message $message): Permission
+    {
+        $this->context->guild = $message->guild;
+        $this->context->channel = $message->channel;
+        $this->context->user = $message->member ?? $message->author;
+        return $this;
+    }
+
+    /**
+     *
+     * How this works:
+     * First we grab all the valid setting->target options that could apply, and then we use check() to cascade down the
+     * list of options. check will return true (allowed), false (disallowed), or null (ignore) for each option.
+     *
+     * @param array $debug
+     *
+     * @return bool
+     * @todo complex permissions with multiple roles
+     *
+     */
+    public function resolve(array &$debug = []): bool
+    {
+        [$settings, $targets] = $this->getValidChecks();
+
+        $carry = $this->default;
+        $debug['default'] = $carry;
+
+        foreach ($settings as $settingType) {
+            $debug[$this->reverseConst($settingType, "SETTING")] = [];
+            foreach ($targets as $targetType) {
+                $res = $this->check($settingType, $targetType);
+                $debug[$this->reverseConst($settingType, "SETTING")][$this->reverseConst($targetType, "TARGET")] = $res;
+                if (!is_null($res)) {
+                    $carry = $res;
+                }
+            }
+        }
+
+        return $carry;
+    }
+
+    private function getValidChecks(): array
+    {
+        $settings = [self::SETTING_GLOBAL];
+        $targets = [self::TARGET_GLOBAL];
+
+        $guild = !is_null($this->context->guild);
+        $channel = !is_null($this->context->channel);
+        $user = !is_null($this->context->user);
+
+        if ($guild) {
+            $settings[] = self::SETTING_GUILD;
+        }
+        if ($channel) {
+            $settings[] = self::SETTING_CHANNEL;
+        }
+        if ($user) {
+            $targets[] = self::TARGET_USER;
+            $targets[] = self::TARGET_BOTADMIN;
+        }
+        if ($user && $guild) {
+            $targets[] = self::TARGET_GUILDOWNER;
+            $targets[] = self::TARGET_ROLE;
+        }
+
+        sort($settings);
+        sort($targets);
+        return [$settings, $targets];
+    }
+
+    private function reverseConst(int $check, string $prefix): string
+    {
+        $reflect = new \ReflectionClass($this);
+        $con = array_filter($reflect->getConstants(), function ($v, $k) use ($prefix) {
+            return stripos($k, $prefix) === 0;
+        }, ARRAY_FILTER_USE_BOTH);
+        return array_search($check, $con);
+    }
+
+    private function check(int $setting, int $target): ?bool
+    {
+        $items = $this->entries->filter(function ($v) use ($setting, $target) {
+            return ($v['settingType'] == $setting) && ($v['targetType'] == $target);
+        });
+
+        // additional filtering
+        switch ($target) {
+            case self::TARGET_USER:
+                $items = $this->filterUser($this->context->user, $items);
+                break;
+            case self::TARGET_ROLE:
+                $items = $this->filterRole($this->context->user, $items);
+                break;
+            case self::TARGET_GLOBAL:
+            case self::TARGET_GUILDOWNER:
+            case self::TARGET_BOTADMIN:
+            default:
+                break;
+        }
+
+        // get this out of the way first
+        if ($items->count() == 0) {
+            return null;
+        }
+
+        // okay now let's turn this into a permissions set.
+        $entry = $items->first();
+        switch ($target) {
+            case self::TARGET_USER:
+            case self::TARGET_GLOBAL:
+                // we dont need to do any fancy checks here, as long as it matches we're good!
+                return (bool) $entry['value'];
+            case self::TARGET_GUILDOWNER:
+                // see if they're a guild owner (admin), othwerwise who gives a fuck
+                $isOwner = $this->context->user->id == $this->context->guild->ownerID;
+                $isAdmin = $this->context->user->permissions->has(Permissions::PERMISSIONS['ADMINISTRATOR']);
+                if ($isOwner || $isAdmin) {
+                    return (bool) $entry['value'];
+                }
+                break;
+            case self::TARGET_BOTADMIN:
+                // bot admin is an easy check
+                if (!in_array($this->context->user->id, $this->huntress->config['evalUsers'])) {
+                    return (bool) $entry['value'];
+                }
+                break;
+            case self::TARGET_ROLE:
+                // we have to get a little recursive with the role check, yikes!
+                return $items->sortCustom(function ($a, $b) {
+                    $aRole = $this->context->user->roles->get($a['target']);
+                    $bRole = $this->context->user->roles->get($b['target']);
+                    return $aRole->position <=> $bRole->position;
+                })->reduce(function ($v) {
+                    return (bool) $v['value'];
+                }, null);
+            default:
+                return null;
+        }
+        return null;
+    }
+
+    private function filterUser(GuildMember $target, Collection $items): Collection
+    {
+        return $items->filter(function ($v) use ($target) {
+            return $target->id == $v['target'];
+        });
+    }
+
+    private function filterRole(GuildMember $target, Collection $items): Collection
+    {
+        return $items->filter(function ($v) use ($target) {
+            return $target->roles->has($v['target']);
+        });
+    }
+
+}
---
+++ src/Huntress/Plugin/Permission.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Copyright (c) 2020 Keira Dueck <sylae@calref.net>
+ * Use of this source code is governed by the MIT license, which
+ * can be found in the LICENSE file.
+ */
+
+namespace Huntress\Plugin;
+
+use GetOpt\ArgumentException;
+use GetOpt\Command;
+use GetOpt\GetOpt;
+use GetOpt\Operand;
+use GetOpt\Option;
+use Huntress\EventData;
+use Huntress\EventListener;
+use Huntress\Huntress;
+use Huntress\Permission as P;
+use Huntress\PluginHelperTrait;
+use Huntress\PluginInterface;
+use React\Promise\PromiseInterface as Promise;
+use Throwable;
+
+
+class Permission implements PluginInterface
+{
+    use PluginHelperTrait;
+
+    public static function register(Huntress $bot)
+    {
+        $bot->eventManager->addEventListener(EventListener::new()
+            ->addCommand("permission")
+            ->addCommand("perm")
+            ->addCommand("hpm")
+            ->setCallback([self::class, "hpm"])
+        );
+    }
+
+    public static function hpm(EventData $data): ?Promise
+    {
+        try {
+            $getOpt = new GetOpt();
+            $getOpt->set(GetOpt::SETTING_SCRIPT_NAME, '!permission');
+            $getOpt->set(GetOpt::SETTING_STRICT_OPERANDS, true);
+            $commands = [];
+            $commands[] = Command::create('check',
+                [self::class, 'checkPerm'])->setDescription('Show the promise resolution chain')->addOperands([
+                (new Operand('permission',
+                    Operand::REQUIRED))->setValidation('is_string')->setDescription('The name of the permission to check.'),
+                (new Operand('user',
+                    Operand::OPTIONAL))->setValidation('is_string')->setDefaultValue("@self")->setDescription('The user whose permissions you want to check. Defaults to self.'),
+            ]);
+            $commands[] = Command::create('add',
+                [self::class, 'addPerm'])->setDescription('Add a permission to the database')->addOperands([
+                (new Operand('permission',
+                    Operand::REQUIRED))->setValidation('is_string')->setDescription('The name of the permission to add.'),
+            ])->addOptions([
+                    (new Option('d', 'deny',
+                        GetOpt::NO_ARGUMENT))->setDescription("Deny the permission instead of allowing it."),
+                    (new Option(null, 'scope',
+                        GetOpt::OPTIONAL_ARGUMENT))->setDefaultValue("guild")->setDescription("Set the scope.")->setArgumentName("global|guild|channel"),
+                    (new Option(null, 'all',
+                        GetOpt::REQUIRED_ARGUMENT))->setDescription("Target all users within the scope."),
+                    (new Option(null, 'role',
+                        GetOpt::REQUIRED_ARGUMENT))->setDescription("Target a particular role, specified as an @mention.")->setArgumentName("@role"),
+                    (new Option(null, 'user',
+                        GetOpt::REQUIRED_ARGUMENT))->setDescription("Target a particular user specified in a format Huntress can understand.")->setArgumentName("username"),
+                    (new Option(null, 'admins',
+                        GetOpt::OPTIONAL_ARGUMENT))->setDescription("Target guild owners and users with the ADMINISTRATOR discord permission."),
+                    (new Option(null, 'evalusers',
+                        GetOpt::OPTIONAL_ARGUMENT))->setDescription("Target bot owners within the scope."),
+                ]
+            );
+            $getOpt->addCommands($commands);
+            try {
+                $args = substr(strstr($data->message->content, " "), 1);
+                $getOpt->process((string) $args);
+            } catch (ArgumentException $exception) {
+                return self::send($data->message->channel, "```\n" . $getOpt->getHelpText() . "```");
+            }
+            $command = $getOpt->getCommand();
+            if (is_null($command)) {
+                return self::send($data->message->channel, "```\n" . $getOpt->getHelpText() . "```");
+            }
+            return call_user_func($command->getHandler(), $getOpt, $data);
+        } catch (Throwable $e) {
+            return self::exceptionHandler($data->message, $e);
+        }
+    }
+
+    public static function checkPerm(GetOpt $getOpt, EventData $data): ?Promise
+    {
+        if ($getOpt->getOperand("user") === "@self") {
+            $user = $data->message->member;
+        } else {
+            $user = self::parseGuildUser($data->guild, $getOpt->getOperand("user"));
+        }
+        if (is_null($user)) {
+            return self::error($data->message, "Unknown User",
+                "Could not recognize the given user. Try using their Tag#1234, or barring that a @mention.");
+        }
+        $p = new P($getOpt->getOperand("permission"), $data->message->client);
+        $p->addMessageContext($data->message);
+        $debug = [];
+        $res = $p->resolve($debug);
+
+        $str = [];
+        $str[] = "Permission `{$p->title}` for user {$user->user->tag}";
+        $str[] = "";
+
+        $end = "(unset)";
+        foreach ($debug as $setting => $targets) {
+            foreach ($targets as $target => $value) {
+                if (is_null($value)) {
+                    $v = "(unset)";
+                } else {
+                    $v = $value ? "**allowed**" : "~~denied~~";
+                    $end = $v;
+                }
+                $str[] = sprintf("Scope `%s` / Target `%s`: %s", $setting, $target, $v);
+            }
+        }
+        $str[] = "";
+        $str[] = "The final value is $end.";
+
+        return $data->message->channel->send(implode(PHP_EOL, $str), ['split' => true]);
+    }
+
+    public static function addPerm(GetOpt $getOpt, EventData $data): ?Promise
+    {
+    }
+}
