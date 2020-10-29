cd0490e47e21a874100e3056d7baddddd32ac954
Keira Dueck <sylae@calref.net>
2020-10-28 23:05:42 -0700

    port in Huntress dice exclusion when known !-using dicebots are around

--- src/Huntress/Plugin/Roll.php
+++ src/Huntress/Plugin/Roll.php
@@ -8,10 +8,13 @@
 namespace Huntress\Plugin;
 
 
+use CharlotteDunois\Yasmin\Models\GuildMember;
+use CharlotteDunois\Yasmin\Models\Permissions;
 use Huntress\DiceHandler;
 use Huntress\EventData;
 use Huntress\EventListener;
 use Huntress\Huntress;
+use Huntress\Permission;
 use Huntress\PluginHelperTrait;
 use Huntress\PluginInterface;
 use React\Promise\PromiseInterface;
@@ -21,6 +24,10 @@ class Roll implements PluginInterface
 {
     use PluginHelperTrait;
 
+    private const KNOWN_DICEBOTS = [
+        261302296103747584, // avrae
+        559331529378103317, // 5ecrawler
+    ];
 
     public static function register(Huntress $bot)
     {
@@ -34,7 +41,23 @@ class Roll implements PluginInterface
     public static function rollHandler(EventData $data): ?PromiseInterface
     {
         try {
+            // don't do anything if another dicebot with the same prefix is here!
+            $count = self::getMembersWithPermission($data->channel,
+                Permissions::PERMISSIONS['SEND_MESSAGES'] | Permissions::PERMISSIONS['VIEW_CHANNEL'])->filter(function (
+                GuildMember $v
+            ) {
+                return in_array($v->id, self::KNOWN_DICEBOTS);
+            })->count();
+            if ($count > 0) {
+                $data->huntress->log->info("Not rolling due to another bot with matching prefix.");
+                return;
+            }
 
+            $p = new Permission("p.dice.roll", $data->huntress, true);
+            $p->addMessageContext($data->message);
+            if (!$p->resolve()) {
+                return $p->sendUnauthorizedMessage($data->message->channel);
+            }
             $string = self::arg_substr($data->message->content, 1);
             $res = DiceHandler::fromString($string);
             $res->member = $data->message->member;
