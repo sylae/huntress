edffafd5f548462dc2d0cc316330628e044da860
Keira Dueck <sylae@calref.net>
2022-01-13 03:21:29 -0800

    use new rep system

--- src/Huntress/Plugin/WormRPFlairs.php
+++ src/Huntress/Plugin/WormRPFlairs.php
@@ -27,6 +27,7 @@ use React\ChildProcess\Process;
 use React\Promise\Deferred;
 use React\Promise\PromiseInterface as Promise;
 use Throwable;
+
 use function React\Promise\reject;
 
 /**
@@ -38,21 +39,23 @@ class WormRPFlairs implements PluginInterface
 {
     use PluginHelperTrait;
 
-    const REP_REGEX = "/([+-]?[0-5X]{1})(A?[A-F])(!?[?+!-]?)/i";
+    const REP_REGEX = "/(A?[A-F])(!?[?+!-$]?)/i";
     const TPL_REGEX = "/\\|\\s*?(%s)\\s*?=\\s*?(.*?)[\\n\\|]/im";
 
     public static function register(Huntress $bot)
     {
-        $bot->eventManager->addEventListener(EventListener::new()
-            ->addCommand("rep")
-            ->addCommand("reputation")
-            ->addGuild(118981144464195584)
-            ->setCallback([self::class, "repHandler"])
+        $bot->eventManager->addEventListener(
+            EventListener::new()
+                ->addCommand("rep")
+                ->addCommand("reputation")
+                ->addGuild(118981144464195584)
+                ->setCallback([self::class, "repHandler"])
         );
-        $bot->eventManager->addEventListener(EventListener::new()
-            ->addCommand("flair")
-            ->addGuild(118981144464195584)
-            ->setCallback([self::class, "flairHandler"])
+        $bot->eventManager->addEventListener(
+            EventListener::new()
+                ->addCommand("flair")
+                ->addGuild(118981144464195584)
+                ->setCallback([self::class, "flairHandler"])
         );
     }
 
@@ -73,18 +76,21 @@ class WormRPFlairs implements PluginInterface
     {
         return $data->message->reply("🔮")->then(function (Message $response) use ($data) {
             try {
-
                 $getOpt = new GetOpt();
                 $getOpt->set(GetOpt::SETTING_SCRIPT_NAME, '!flair');
                 $getOpt->set(GetOpt::SETTING_STRICT_OPERANDS, true);
 
-                $getOpt->addOperand((new Operand('color', Operand::OPTIONAL))
-                    ->setValidation('is_string')
-                    ->setDescription("The color (CSS class) you want your flair to be. Consult the pins in <#118981977935183873> for options.")
+                $getOpt->addOperand(
+                    (new Operand('color', Operand::OPTIONAL))
+                        ->setValidation('is_string')
+                        ->setDescription(
+                            "The color (CSS class) you want your flair to be. Consult the pins in <#118981977935183873> for options."
+                        )
                 );
                 if ($data->message->member->roles->has(456321111945248779)) {
-                    $getOpt->addOption((new Option('u', 'user', GetOpt::OPTIONAL_ARGUMENT))
-                        ->setDescription("(Staff) Specify a different user.")
+                    $getOpt->addOption(
+                        (new Option('u', 'user', GetOpt::OPTIONAL_ARGUMENT))
+                            ->setDescription("(Staff) Specify a different user.")
                     );
                 }
 
@@ -108,12 +114,16 @@ class WormRPFlairs implements PluginInterface
                 // get the user's flair...
                 $redditUser = self::fetchRedditAccount($member);
                 if (is_null($redditUser)) {
-                    return $response->edit("Sorry, I couldn't find your reddit account. Please bug a staff member to run the following command:\n" .
-                        "`!linkAccount your_reddit_name_with_no_/u/_prefix \"{$member->displayName}\"`");
+                    return $response->edit(
+                        "Sorry, I couldn't find your reddit account. Please bug a staff member to run the following command:\n" .
+                        "`!linkAccount your_reddit_name_with_no_/u/_prefix \"{$member->displayName}\"`"
+                    );
                 }
 
                 // get the flair from the wiki!
-                $wikiURL = "https://wormrp.syl.ae/w/api.php?action=parse&format=json&text=%7B%7Bflair%7C%2Fu%2F" . urlencode($redditUser) . "%7D%7D&contentmodel=wikitext";
+                $wikiURL = "https://wormrp.syl.ae/w/api.php?action=parse&format=json&text=%7B%7Bflair%7C%2Fu%2F" . urlencode(
+                        $redditUser
+                    ) . "%7D%7D&contentmodel=wikitext";
                 $chain = URLHelpers::getHTTPClient()->get($wikiURL)->then(function (ResponseInterface $response) {
                     // get new flair tag
                     $text = json_decode((string)$response->getBody())->parse->text->{'*'};
@@ -176,7 +186,6 @@ class WormRPFlairs implements PluginInterface
                 });
 
                 return $chain;
-
             } catch (Throwable $e) {
                 return self::exceptionHandler($data->message, $e);
             }
@@ -206,20 +215,35 @@ class WormRPFlairs implements PluginInterface
             $getOpt->set(GetOpt::SETTING_SCRIPT_NAME, '!rep');
             $getOpt->set(GetOpt::SETTING_STRICT_OPERANDS, true);
 
-            $getOpt->addOperand((new Operand('character',
-                Operand::REQUIRED))->setValidation('is_string'));
-            $getOpt->addOperand((new Operand('rep',
-                Operand::REQUIRED))->setValidation('is_string'));
+            $getOpt->addOperand(
+                (new Operand(
+                    'character',
+                    Operand::REQUIRED
+                ))->setValidation('is_string')
+            );
+            $getOpt->addOperand(
+                (new Operand(
+                    'rep',
+                    Operand::REQUIRED
+                ))->setValidation('is_string')
+            );
 
             try {
                 $args = substr(strstr($message->content, " "), 1);
                 $getOpt->process((string)$args);
             } catch (ArgumentException $exception) {
-                return $data->message->reply($getOpt->getHelpText() . "Note: if you're getting this but everything looks correct, try `!rep -- <character> <rep>` instead.");
+                return $data->message->reply(
+                    $getOpt->getHelpText(
+                    ) . "Note: if you're getting this but everything looks correct, try `!rep -- <character> <rep>` instead."
+                );
             }
 
-            return self::editRep($getOpt->getOperand("character"), $getOpt->getOperand("rep"),
-                $message->client, $data)->then(function (
+            return self::editRep(
+                $getOpt->getOperand("character"),
+                $getOpt->getOperand("rep"),
+                $message->client,
+                $data
+            )->then(function (
                 $res
             ) use ($message) {
                 return $res['myMessage']->edit("`{$res['wikipage']}` has been updated.");
@@ -227,8 +251,6 @@ class WormRPFlairs implements PluginInterface
             }, function ($e) use ($message) {
                 return self::error($message, "Editing rep failed!", $e);
             });
-
-
         } catch (Throwable $e) {
             return self::exceptionHandler($message, $e);
         }
@@ -248,7 +270,7 @@ class WormRPFlairs implements PluginInterface
 
             $matches = [];
             preg_match(self::REP_REGEX, $rep, $matches);
-            if (count($matches) != 4) {
+            if (count($matches) != 3) {
                 return reject("Invalid rep string `$rep`.");
             }
             $cd['desiredRep'] = $matches;
@@ -260,8 +282,10 @@ class WormRPFlairs implements PluginInterface
             return self::login($bot, $cd);
         })->then(function (array $cd) use ($bot) {
             // grab a csrf token for the edit
-            return URLHelpers::getHTTPClient()->get("https://wormrp.syl.ae/w/api.php?action=query&meta=tokens&format=json",
-                ['Cookie' => self::cookieString()])->then(function (
+            return URLHelpers::getHTTPClient()->get(
+                "https://wormrp.syl.ae/w/api.php?action=query&meta=tokens&format=json",
+                ['Cookie' => self::cookieString()]
+            )->then(function (
                 ResponseInterface $response
             ) use ($cd) {
                 // first we get a csrf token...
@@ -273,8 +297,12 @@ class WormRPFlairs implements PluginInterface
             // actually edit
             return self::editPage($cd, $char);
         })->then(function (array $cd) use ($char) {
-            return URLHelpers::getHTTPClient()->get("https://wormrp.syl.ae/w/api.php?action=parse&format=json&text=%7B%7Bflair%7C%2Fu%2F" . urlencode($cd['reddituser']) . "%7D%7D&contentmodel=wikitext",
-                ['Cookie' => self::cookieString()])->then(function (
+            return URLHelpers::getHTTPClient()->get(
+                "https://wormrp.syl.ae/w/api.php?action=parse&format=json&text=%7B%7Bflair%7C%2Fu%2F" . urlencode(
+                    $cd['reddituser']
+                ) . "%7D%7D&contentmodel=wikitext",
+                ['Cookie' => self::cookieString()]
+            )->then(function (
                 ResponseInterface $response
             ) use ($cd) {
                 // get new flair tag
@@ -293,8 +321,12 @@ class WormRPFlairs implements PluginInterface
 
     private static function getCanonicalPostTitle(array $cd, string $char)
     {
-        return URLHelpers::resolveURLToData("https://wormrp.syl.ae/w/api.php?action=ask&format=json&api_version=3&query=[[Identity::" . urlencode($char) . "]]|?Author",
-            ['Cookie' => self::cookieString()])->then(function (
+        return URLHelpers::resolveURLToData(
+            "https://wormrp.syl.ae/w/api.php?action=ask&format=json&api_version=3&query=[[Identity::" . urlencode(
+                $char
+            ) . "]]|?Author",
+            ['Cookie' => self::cookieString()]
+        )->then(function (
             string $data
         ) use ($char, $cd) {
             $data = json_decode($data)->query->results;
@@ -302,7 +334,9 @@ class WormRPFlairs implements PluginInterface
                 return reject("Multiple characters matching that name. This shouldn't happen, please @ keira.");
             }
             if (count($data) == 0) {
-                return reject("I didn't find anyone with that name. Full list of valid names here: <https://wormrp.syl.ae/wiki/Property:Identity>");
+                return reject(
+                    "I didn't find anyone with that name. Full list of valid names here: <https://wormrp.syl.ae/wiki/Property:Identity>"
+                );
             }
 
             key($data[0]);
@@ -323,7 +357,6 @@ class WormRPFlairs implements PluginInterface
             $x[] = $k . "=" . $v['data'];
         }
         return implode("; ", $x);
-
     }
 
     private static function cookiejar(array $set = null): array
@@ -340,15 +373,19 @@ class WormRPFlairs implements PluginInterface
         $browser = URLHelpers::getHTTPClient();
 
         // first see if we're already logged in, makes shit mucho easier :)
-        return $browser->get("https://wormrp.syl.ae/w/api.php?action=query&meta=userinfo&format=json",
-            ['Cookie' => self::cookieString()])->then(function (
+        return $browser->get(
+            "https://wormrp.syl.ae/w/api.php?action=query&meta=userinfo&format=json",
+            ['Cookie' => self::cookieString()]
+        )->then(function (
             ResponseInterface $response
         ) use ($cd, $browser, $bot) {
             $cd['userinfo'] = json_decode((string)$response->getBody())->query->userinfo;
             if (array_key_exists('anon', (array)$cd['userinfo'])) {
                 // actually log in!
-                return $browser->get("https://wormrp.syl.ae/w/api.php?action=query&meta=tokens&type=login&format=json",
-                    ['Cookie' => self::cookieString()])->then(function (
+                return $browser->get(
+                    "https://wormrp.syl.ae/w/api.php?action=query&meta=tokens&type=login&format=json",
+                    ['Cookie' => self::cookieString()]
+                )->then(function (
                     ResponseInterface $response
                 ) use ($cd) {
                     // first we get a login token...
@@ -357,14 +394,17 @@ class WormRPFlairs implements PluginInterface
                     return $cd;
                 })->then(function (array $cd) use ($bot, $browser) {
                     // then we actually log in!
-                    return $browser->submit("https://wormrp.syl.ae/w/api.php",
+                    return $browser->submit(
+                        "https://wormrp.syl.ae/w/api.php",
                         [
                             'action' => 'login',
                             'format' => 'json',
                             'lgname' => $bot->config['wiki']['username'],
                             'lgpassword' => $bot->config['wiki']['password'],
                             'lgtoken' => $cd['logintoken'],
-                        ], ['Cookie' => self::cookieString()])->then(function (
+                        ],
+                        ['Cookie' => self::cookieString()]
+                    )->then(function (
                         ResponseInterface $response
                     ) use ($cd) {
                         self::setCookiesIfApplicable($response);
@@ -378,7 +418,6 @@ class WormRPFlairs implements PluginInterface
                 return $cd;
             }
         });
-
     }
 
     private static function setCookiesIfApplicable(ResponseInterface $response)
@@ -397,13 +436,16 @@ class WormRPFlairs implements PluginInterface
             }
             self::cookiejar($jar);
         }
-
     }
 
     private static function editPage(array $cd, string $char)
     {
-        return URLHelpers::resolveURLToData("https://wormrp.syl.ae/w/api.php?action=query&format=json&prop=revisions&titles=" . urlencode($cd['wikipage']) . "&rvprop=content&rvslots=main",
-            ['Cookie' => self::cookieString()])->then(function (
+        return URLHelpers::resolveURLToData(
+            "https://wormrp.syl.ae/w/api.php?action=query&format=json&prop=revisions&titles=" . urlencode(
+                $cd['wikipage']
+            ) . "&rvprop=content&rvslots=main",
+            ['Cookie' => self::cookieString()]
+        )->then(function (
             string $data
         ) use ($char, $cd) {
             $data = (array)json_decode($data)->query->pages;
@@ -424,15 +466,19 @@ class WormRPFlairs implements PluginInterface
                 return reject("Could not find template parameters in source wikipage. Please @ keira");
             }
             $c = $cd['content'];
-            $c = preg_replace(sprintf(self::TPL_REGEX, "rep_morality"), '|$1 = ' . $cd['desiredRep'][1] . PHP_EOL, $c);
-            $c = preg_replace(sprintf(self::TPL_REGEX, "rep_notoriety"),
-                '|$1 = ' . mb_strtoupper($cd['desiredRep'][2]) . PHP_EOL, $c);
-            $c = preg_replace(sprintf(self::TPL_REGEX, "rep_criminal"), '|$1 = ' . $cd['desiredRep'][3] . PHP_EOL, $c);
+            // $c = preg_replace(sprintf(self::TPL_REGEX, "rep_morality"), '|$1 = ' . $cd['desiredRep'][1] . PHP_EOL, $c);
+            $c = preg_replace(
+                sprintf(self::TPL_REGEX, "rep_notoriety"),
+                '|$1 = ' . mb_strtoupper($cd['desiredRep'][1]) . PHP_EOL,
+                $c
+            );
+            $c = preg_replace(sprintf(self::TPL_REGEX, "rep_criminal"), '|$1 = ' . $cd['desiredRep'][2] . PHP_EOL, $c);
             $cd['newContent'] = $c;
             return $cd;
         })->then(function (array $cd) {
             // then we actually, fucking finally, edit the page!
-            return URLHelpers::getHTTPClient()->submit("https://wormrp.syl.ae/w/api.php",
+            return URLHelpers::getHTTPClient()->submit(
+                "https://wormrp.syl.ae/w/api.php",
                 [
                     'action' => 'edit',
                     'format' => 'json',
@@ -445,7 +491,9 @@ class WormRPFlairs implements PluginInterface
                     'contentformat' => 'text/x-wiki',
                     'contentmodel' => 'wikitext',
                     'token' => $cd['csrf'],
-                ], ['Cookie' => self::cookieString()])->then(function (
+                ],
+                ['Cookie' => self::cookieString()]
+            )->then(function (
                 ResponseInterface $response
             ) use ($cd) {
                 self::setCookiesIfApplicable($response);
